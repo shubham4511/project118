bcfc6a281968abb10e340e27116a262cbab1ae44
shubham4511 <89202701+shubham4511@users.noreply.github.com>
2022-08-29 18:58:12 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 quick_draw_data_set=["aircraft carrier","airplane","alarm clock","ambulance","angel","animal migration","ant","anvil","apple","arm","asparagus","axe","backpack","banana","bandage","barn","baseball","baseball bat","basket","basketball","bat","bathtub","beach","bear","beard","bed","bee","belt","bench","bicycle","binoculars","bird","birthday cake","blackberry","blueberry","book","boomerang","bottlecap","bowtie","bracelet","brain","bread","bridge","broccoli","broom","bucket","bulldozer","bus","bush","butterfly","cactus","cake","calculator","calendar","camel","camera","camouflage","campfire","candle","cannon","canoe","car","carrot","castle","cat","ceiling fan","cello","cell phone","chair","chandelier","church","circle","clarinet","clock","cloud","coffee cup","compass","computer","cookie","cooler","couch","cow","crab","crayon","crocodile","crown","cruise ship","cup","diamond","dishwasher","diving board","dog","dolphin","donut","door","dragon","dresser","drill","drums","duck","dumbbell","ear", "elbow","elephant","envelope","eraser","eye","eyeglasses","face","fan","feather","fence","finger","fire hydrant","fireplace","firetruck","fish","flamingo","flashlight","flip flops","floor lamp","flower","flying saucer","foot","fork","frog","frying pan","garden","garden hose","giraffe","goatee","golf club","grapes","grass","guitar","hamburger","hammer","hand","harp","hat","headphones","hedgehog","helicopter","helmet","hexagon","hockey puck","hockey stick","horse","hospital","hot air balloon","hot dog","hot tub","hourglass","house","house plant","hurricane","ice cream","jacket","jail","kangaroo","key","keyboard","knee","knife","ladder","lantern","laptop","leaf","leg","light bulb","lighter","lighthouse","lightning","line","lion","lipstick","lobster","lollipop","mailbox","map","marker","matches","megaphone","mermaid","microphone","microwave","monkey","moon","mosquito","motorbike","mountain","mouse","moustache","mouth","mug","mushroom","nail","necklace","nose","ocean","octagon","octopus","onion","oven","owl","paintbrush","paint can","palm tree","panda","pants","paper clip","parachute","parrot","passport","peanut","pear","peas","pencil","penguin","piano","pickup truck","picture frame","pig","pillow","pineapple","pizza","pliers","police car","pond","pool","popsicle","postcard","potato","power outlet","purse","rabbit","raccoon","radio","rain","rainbow","rake","remote control","rhinoceros","rifle","river","roller coaster","rollerskates","sailboat","sandwich","saw","saxophone","school bus","scissors","scorpion","screwdriver","sea turtle","see saw","shark","sheep","shoe","shorts","shovel","sink","skateboard","skull","skyscraper","sleeping bag","smiley face","snail","snake","snorkel","snowflake","snowman","soccer ball","sock","speedboat","spider","spoon","spreadsheet","square","squiggle","squirrel","stairs","star","steak","stereo","stethoscope","stitches","stop sign","stove","strawberry","streetlight","string bean","submarine","suitcase","sun","swan","sweater","swingset","sword","syringe","table","teapot","teddy-bear","telephone","television","tennis racquet","tent","The Eiffel Tower","The Great Wall of China","The Mona Lisa","tiger","toaster","toe","toilet","tooth","toothbrush","toothpaste","tornado","tractor","traffic light","train","tree","triangle","trombone","truck","trumpet","tshirt","umbrella","underwear","van","vase","violin","washing machine","watermelon","waterslide","whale","wheel","windmill","wine bottle","wine glass","wristwatch","yoga","zebra","zigzag"]
-random_no = Math.floor((Math.random()*array_1.length)+1);
-Element_of_array = array_1[random_nomber];
+random_no = Math.floor((Math.random()*quick_draw_data_set.length)+1);
+Element_of_array = quick_draw_data_set[random_no];
 document.getElementById("sketch_to_be_drawn").innerHTML="Sketch to Be Drawn"+Element_of_array;
 
-timer_countent=0;
+timer_counter=0;
 timer_check="";
 drawn_sketch="";
 answer_holder="";
 score=0;
 sketch = Element_of_array
 
-function prelode(){
-    Classifier=ml5.imageClassifier("Doodlenet");
+function preload(){
+    classifier=ml5.imageClassifier("Doodlenet");
 }
 function draw(){
     strokeWeight(13);
     stroke(0);
-    if(mouseIspressed){
+    if(mouseIsPressed){
     line(pmouseX,pmouseY,mouseX,mouseY);
     }
     check_sketch();
@@ -28,13 +28,13 @@ function draw(){
 
 }
 function check_sketch(){
-    timer_countent++
+    timer_counter++
     document.getElementById("Timer").innerHTML="Timer:"+timer_counter;
-    if(timer_countent>400){
+    if(timer_counter>400){
         document.getElementById("your_sketch").innerHTML="Your Sketch:";
         ducument.getElementById("confidence").innerHTML="Confidence:";
 
-        timer_countent=0;
+        timer_counter=0;
         timer_check="completed";
 
 
@@ -48,8 +48,8 @@ function check_sketch(){
 }
 function updateCanvas(){
     background("white");
-    random_no = Math.floor((Math.random()*array_1.length)+1);
-    Element_of_array = array_1[random_nomber];
+    random_no = Math.floor((Math.random()*quick_draw_data_set.length)+1);
+    Element_of_array = quick_draw_data_set[random_no];
     document.getElementById("sketch_to_be_drawn").innerHTML="Sketch to Be Drawn"+Element_of_array;
 
 }
@@ -57,13 +57,13 @@ function setup(){
     canvas=createCanvas(280,280);
     canvas.center();
     background("white");
-    canvas.mouseReleased(Classifycanvas);
+    canvas.mouseReleased(classifyCanvas);
 }
-function Classifycanvas(){
-    Classifier.Classify(canvas,gotResults);
+function classifyCanvas(){
+    classifier.classify(canvas,gotResults);
 
 }
-function gotResults(error,Results){
+function gotResults(error,results){
     if(error){
         console.error(error);
 
@@ -71,5 +71,5 @@ function gotResults(error,Results){
     console.log(results);
     drawn_sketch=results[0].label;
     document.getElementById("your_sketch").innerHTML="Your Sketch:"+results[0].label;
-    ducument.getElementById("confidence").innerHTML="Confidence:"+Math.round(results[0].confidence*100)+'%';
+    document.getElementById("confidence").innerHTML="Confidence:"+Math.round(results[0].confidence*100)+'%';
 }
